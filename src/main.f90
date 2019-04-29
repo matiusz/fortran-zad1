e9726821c28ecb574f15580fcf2a2d84da28b36d
matiusz <37378034+matiusz@users.noreply.github.com>
2019-04-29 19:46:27 +0200

    All modules, main with test

--- src/main.f90
+++ src/main.f90
@@ -1,21 +1,43 @@
 program main
 	use naivemath
+	use bettermath
+	use dotmath
+	real (kind = 16), dimension(:,:), allocatable :: i, j
+	real :: startnaiv, finishnaiv, startbett, finishbett, startdot, finishdot, startmat, finishmat
+	real, dimension(:,:), allocatable :: C
+	integer, dimension(8) :: matrix_sizes
+	matrix_sizes(1) = 10
+	do k = 2, size(matrix_sizes)
+		matrix_sizes(k) = matrix_sizes(k-1)*2
+	end do
+	write (*,*) "N	, Naive	 , Better ,	Dot,	Mat"
+	do k = 1, size(matrix_sizes)
+		allocate(i(matrix_sizes(k), matrix_sizes(k)))
+		allocate(j(matrix_sizes(k), matrix_sizes(k)))
+		i=1
+		j=2
 
-	real, dimension(2, 3) :: i
-	real, dimension(3,2) :: j
-	i(1, 1) = 1
-	i(1, 2) = 2
-	i(1, 3) = 3
-	i(2, 1) = 4
-	i(2, 2) = 5
-	i(2, 3) = 6
-	j(1, 1) = 11
-	j(1, 2) = 12
-	j(2, 1) = 13
-	j(2, 2) = 14
-	j(3, 1) = 15
-	j(3, 2) = 16
-	write(*,*) i
-	write(*,*) j
-	write(*,*) naivmull(i, j)
+		call cpu_time(startnaiv)
+		C = naivmull(i, j)
+		call cpu_time(finishnaiv)
+
+		call cpu_time(startbett)
+		C = bettmull(i, j)
+		call cpu_time(finishbett)
+
+		call cpu_time(startdot)
+		C = dotmull(i, j)
+		call cpu_time(finishdot)
+
+		call cpu_time(startmat)
+		C = matmul(i, j)
+		call cpu_time(finishmat)
+		write (*,9001) matrix_sizes(k), ",", finishnaiv-startnaiv, ",", &
+		finishbett-startbett,",", finishdot-startdot,",", finishmat-startmat
+		9001 format (I8, A, F8.5, A, F8.5, A, F8.5, A, F8.5)
+		!write (*,*) C
+
+		deallocate(i)
+		deallocate(j)
+	end do
 end program
